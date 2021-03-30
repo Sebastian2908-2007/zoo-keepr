65bc0bf9297aa4b5719582a9dac6e68d3ba43459
sebastian <sebastian bowen>
2021-03-30 16:45:12 -0600

    have front end set up and connected to server

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(express.urlencoded({extended: true}));
 // parse incoming JSON data
 app.use(express.json());
 const {animals} = require('./data/animals');
+// middleware for script and css
+app.use(express.static('public'));
 
 
 
@@ -101,6 +103,22 @@ app.get('/api/animals/:id', (req, res) => {
   } else {
     res.send(404);
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+   res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res)=> {
+res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.post('/api/animals', (req, res) => {
